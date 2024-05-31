ed7ba36431abfab809ea6942769e13bb9dfa780f
Akhil Nautiyal <akhilnautiyal5@gmail.com>
2024-06-01 00:09:01 +0530

    second change #2

--- ytvideo.py
+++ ytvideo.py
@@ -1,22 +1,33 @@
 import tkinter
 import customtkinter
 from pytube import YouTube
+class Main:
+    def __init__(self):
+        # System Settings
+        customtkinter.set_appearance_mode("System")
+        customtkinter.set_default_color_theme("blue")
 
-# System Settings
-customtkinter.set_appearance_mode("System")
-customtkinter.set_default_color_theme("blue")
+        # Our app frame
+        app = customtkinter.CTk()
+        app.geometry("720x480")
+        app.title("Youtube Video Dowloader")
 
-# Our app frame
-app = customtkinter.CTk()
-app.geometry("720x480")
-app.title("Youtube Video Dowloader")
+        # Adding UI Elements
+        title = customtkinter.CTkLabel(app, text="Insert the youtube link")
+        title.pack(padx=10, pady=10)
 
-# Adding UI Elements
-title = customtkinter.CTkLabel(app, text="Insert the youtube link")
-title.pack(padx=10, pady=10)
+        # Liink input
+        url_var = tkinter.StringVar()
+        link = customtkinter.CTkEntry(app, width=350, height=20, textvariable=url_var)
+        link.pack()
+        print (url_var)
 
-# Liink input
-link = customtkinter.CTkEntry(app, width=350, height=40, )
+        # Download button
+        # download = customtkinter.CTkButton(app, text="Download", command=startDownload)
 
-# Run app
-app.mainloop()
+        # Run app
+        app.mainloop()
+
+if __name__ == "__main__":
+    main_app = Main()
+
