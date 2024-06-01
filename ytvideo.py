aa137309b01734314083f864434028dcbd2cc52c
Akhil Nautiyal <akhilnautiyal5@gmail.com>
2024-06-02 00:23:25 +0530

    commit 5-appreance change button

--- ytvideo.py
+++ ytvideo.py
@@ -6,11 +6,13 @@ import os
 class Main:
     def __init__(self):
         self.main()
+
     
     def on_enter(self, event):
         print("Enter key pressed")
         self.startDownload()
 
+    
     def startDownload(self):
         ytLink = self.url_var.get()  # Correctly get the value from url_var
         output_folder = os.path.join(os.path.expanduser("~"), "Videos", "yt_playlist")
@@ -22,16 +24,21 @@ class Main:
                 ytObject = YouTube(ytLink)
                 video = ytObject.streams.get_highest_resolution()
                 video.download(output_path=output_folder)
-                print(f"Downloaded: {ytObject.title}")
+                self.finishedLabel.configure(text=f"Downloading: {ytObject.title}")
             except Exception as e:
-                print(f"Error: {e}")
-            self.finishedLabel.configure("Gownloaded!")
+                self.finishedLabel.configure(text=f"Error: {e}")
+                
+                self.finishedLabel.configure(text=f"")
+                
+            self.finishedLabel.configure(text=f"Downloading: {ytObject.title}")
         else:
-            print("Enter Valid link")
+            self.finishedLabel.configure(text="Enter Valid link", text_color="red")
 
     def main(self):
+
         # System Settings
         customtkinter.set_appearance_mode("System")
+        customtkinter.set_appearance_mode("dark")
         customtkinter.set_default_color_theme("blue")
 
         # Our app frame
@@ -39,9 +46,13 @@ class Main:
         self.app.geometry("720x480")
         self.app.title("YouTube Video Downloader")
 
+        #font family, size, weight=bold/normal, slant=italic/roman
+        title_font = customtkinter.CTkFont(family="Helvetica", size=30, weight="bold", slant="roman", underline=True, overstrike=False)
+
+
         # Adding UI Elements
-        title = customtkinter.CTkLabel(self.app, text="Insert the YouTube link")
-        title.pack(padx=10, pady=10)
+        title = customtkinter.CTkLabel(self.app, text="Youtube video Downloader", font=title_font)
+        title.pack(padx=10, pady=50)
 
         # Link input
         self.url_var = tkinter.StringVar()
@@ -55,11 +66,18 @@ class Main:
         download = customtkinter.CTkButton(self.app, text="Download", command=self.startDownload)
         download.pack(padx=10, pady=10)
 
+        appreance_button =customtkinter.CTkOptionMenu(self.app, values=["Light", "Dark", "System"], command=self.change_appearance_mode_event)
+        appreance_button.pack(pady=20)
+
+
         # Bind the Enter key to the startDownload function
         self.app.bind('<Return>', self.on_enter)
 
         # Run app
         self.app.mainloop()
+    
+    def change_appearance_mode_event(self, new_appearance_mode):
+        customtkinter.set_appearance_mode(new_appearance_mode)
 
 if __name__ == "__main__":
     print("App starting...")
